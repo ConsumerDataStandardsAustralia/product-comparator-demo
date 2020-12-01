b56380a8ea5a0949d3d1dbe349d389f400366bfd
Vadym <30234255+vadkor@users.noreply.github.com>
2020-12-01 12:17:31 +1100

    Fixed eslint warnings

--- src/store/comparison/reducer.js
+++ src/store/comparison/reducer.js
@@ -1,6 +1,6 @@
 import { COMPARE_PRODUCTS } from './actions'
 
-export default function(state=[], action) {
+export default function comparison(state=[], action) {
   if (action.type === COMPARE_PRODUCTS) {
     return [...action.payload]
   } else {
